e827729e9f86be39874a5de802c10f2daa291062
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2021-01-21 18:08:55 +0100

    ajout des dernières fonctionnalités qu'il manquait (testRegression, dessinerRectangle), reste quand meme a écrire effacer et le Makefile pour compile le tout.

--- TD_moduleImage/11805901_11805901_11805901/src/Image.cpp
+++ TD_moduleImage/11805901_11805901_11805901/src/Image.cpp
@@ -33,7 +33,11 @@ void Image::setPix (unsigned int x,unsigned int y, Pixel couleur) {
 }
 
 void Image::dessinerRectangle (unsigned int Xmin,unsigned int Ymin,unsigned int Xmax,unsigned int Ymax, Pixel couleur) {
-
+    setPix(Xmin,Ymin, couleur); // On fixe le bas à gauche du rectangle.
+    setPix(Xmin,Ymax, couleur); // On fixe le haut à gauche du rectangle.
+    
+    setPix(Xmax,Ymax, couleur); // On fixe le haut à droite du rectangle.
+    setPix(Xmax,Ymin, couleur); // On fixe le bas à droite du rectangle.
 }
 
 void Image::effacer (Pixel couleur) {
@@ -41,5 +45,23 @@ void Image::effacer (Pixel couleur) {
 }
 
 void Image::testRegression() {
+    Image Im; 
+    Pixel couleurRouge(255, 0, 0); 
+
+    Image im(114, 29);
+
+    cout << "Info du pixel dans l'image im aux coordo (100,15) :" << endl;
+    im.getPix(100, 15);
+    cout << endl;
+    cout << "Info du pixel dans l'image Im aux coordo (0,0) :" << endl;
+    Im.getPix(0, 0);
+    cout << endl;
+
+    cout << "On va set le pixel en (100,15) en rouge : " << endl;
+    im.setPix(100, 15, couleurRouge);
+    cout << endl;
+    cout << "On set le pixel de Im en rouge" << endl;
+    Im.setPix(0, 0, couleurRouge);
+    cout << endl;
 
 }
